26ae4edb6515b0d1abe84224a08f6fef8d9da138
BAHAR HAMZA <baharhamza2000@gmail.com>
2019-07-09 15:58:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+class Game extends Comment {
+  render() {
+    return (
+    <div className= "style">
+    
+  <div>
+    put the playerCard Componeents here
+  </div>
+  <div className="winner"></div>
+  <button type="button">Play the Game</button>
+  </div>
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
 }
 
-export default App;
+export default Game;
